20c6a78b98f882f77df29a7338cfd3d1c7cb8c74
Max Soljak <max.soljak@gmail.com>
2025-02-11 13:48:40 -0500

    update

--- lab04.py
+++ lab04.py
@@ -8,7 +8,8 @@ elif x == "bad":
     print("Oh no whats wrong")
     y = input("Can I help you somehow?")
     if y == "yes":
-        a == input("How can I help you?")
-        
+        a = input("How can I help you?")
+        if a == "i dont know":
+            print("Thats okay, sometimes we dont know what is really wrong")
     elif y == "no":
         exit()
